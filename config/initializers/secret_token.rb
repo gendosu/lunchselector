83aba20c449df542aa973b8a6f0f2449c9a10516
Gen Takahashi <gendosu@gmail.com>
2014-07-28 22:58:20 +0000

    初期登録

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lunchselector::Application.config.secret_key_base = 'b8aa0b2013454dfaab9c47d97b614ac96cc9f9d825d90145312dfd839947249bffd7ecfaf26aa7f17ec6d6827f4074ada855ae34058992d1c81f8647140873cc'
